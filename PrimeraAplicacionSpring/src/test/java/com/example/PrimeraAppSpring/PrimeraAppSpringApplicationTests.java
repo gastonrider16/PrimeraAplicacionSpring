b752676fc78c88b97084191fd7be26eb01edd77b
gastonrider16 <gastonrider16@gmail.com>
2021-08-11 20:18:48 -0300

    Version 1.0 Sin funciionar en cualquier ide

---
+++ PrimeraAplicacionSpring/src/test/java/com/example/PrimeraAppSpring/PrimeraAppSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PrimeraAppSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeraAppSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
