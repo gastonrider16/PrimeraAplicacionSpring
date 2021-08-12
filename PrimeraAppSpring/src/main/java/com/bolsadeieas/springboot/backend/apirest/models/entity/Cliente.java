6da7189d454a0e5498ccd6669689f91e4ba92588
gastonrider16 <gastonrider16@gmail.com>
2021-08-11 21:05:34 -0300

    Video 35 No reconoce libreria javax.persistence

---
+++ PrimeraAppSpring/src/main/java/com/bolsadeieas/springboot/backend/apirest/models/entity/Cliente.java
@@ -0,0 +1,53 @@
+package com.bolsadeieas.springboot.backend.apirest.models.entity;
+
+import com.sun.javafx.beans.IDProperty;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+//NO ME RECONOCE javax.persistence
+
+@Entity
+@Table(name="clientes")
+public class Cliente implements Serializable {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String email;
+    @Column(name="create_at")
+    @Temporal(TemporalType.DATE)
+    private Date createAt;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido= apellido;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public Date getCreateAt() {
+        return createAt;
+    }
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+}
